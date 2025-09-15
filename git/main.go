f244a13924d487f98002fac556b0c718fb6ac3e7
Robert Petz <rpetz@ignitelabs.net>
2025-09-14 18:31:32 -0700

    Initial check in

---
+++ git/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "git.ignitelabs.net/navigator/netscape"
+
+func main() {
+	netscape.GitVanity.Navigate("https://github.com/ignite-laboratories")
+}
---
+++ ignite/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "git.ignitelabs.net/navigator/netscape"
+
+func main() {
+	netscape.IgniteLabs.Navigate()
+}
